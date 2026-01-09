09654abd25d3e59f08be598260c950d8f6ca2bf6
Taofeeq Jimoh <dicksonanderson2004@gmail.com>
2026-01-09 21:52:36 +0100

    chore: Add sessions table schema for persistent sessions

---
+++ database/sessions_table.sql
@@ -0,0 +1,7 @@
+-- Save this as database/sessions_table.sql
+CREATE TABLE IF NOT EXISTS sessions (
+  session_id varchar(128) COLLATE utf8mb4_bin NOT NULL,
+  expires int(11) unsigned NOT NULL,
+  data mediumtext COLLATE utf8mb4_bin,
+  PRIMARY KEY (session_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
